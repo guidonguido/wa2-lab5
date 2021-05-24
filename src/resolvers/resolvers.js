2944c34f3e2dbe3ba033d251673445c3cee91521
Ch1Ale <66998114+Ch1Ale@users.noreply.github.com>
2021-05-24 18:45:44 +0200

    complete filter's and sort's logic on query resolver

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,5 +1,6 @@
 import Product from "../model/Product.js";
 import Comment from "../model/Comment.js";
+import {filterSchema} from "graphql-tools";
 
 const resolvers = {
     Query: {
@@ -28,6 +29,64 @@ const resolvers = {
         products: (parent, args, context, info) => {
 
             return Product.find()
+                .then((products) => {
+
+                    const {filter, sort} = args
+                    const shouldApplyFilters = filter !== undefined
+                    const shouldApplySorting = sort !== undefined
+
+                    console.log(`FILTER: ${shouldApplyFilters}`)
+                    console.log(`SORTING: ${shouldApplySorting} - ${sort.value} , ${sort.order}`)
+
+
+                    if(shouldApplyFilters){
+                        const shouldApplyCategoryFilter = filter.categories !== undefined
+                        const shouldApplyMinStarsFilter = filter.minStars !== undefined
+                        const shouldApplyMinPriceFilter = filter.minPrice !== undefined
+                        const shouldApplyMaxPriceFilter = filter.maxPrice !== undefined
+
+                        console.log(`FILTER cat: ${shouldApplyCategoryFilter} - ${filter.categories}`)
+                        console.log(`FILTER stars: ${shouldApplyMinStarsFilter} - ${filter.minStars}`)
+                        console.log(`FILTER min price: ${shouldApplyMinPriceFilter} - ${filter.minPrice}`)
+                        console.log(`FILTER max price: ${shouldApplyMaxPriceFilter} - ${filter.maxPrice}`)
+
+                        if(shouldApplyCategoryFilter)
+                            products = products.filter( (p) => filter.categories.includes(p.category) )
+
+                        if(shouldApplyMinPriceFilter)
+                            products = products.filter( (p) => p.price >= filter.minPrice )
+
+                        if(shouldApplyMaxPriceFilter)
+                            products = products.filter( (p) => p.price <= filter.maxPrice )
+
+                        if(shouldApplyMinStarsFilter)
+                            products = products.filter( (p) => p.stars >= filter.minStars)
+
+                        if(shouldApplySorting){
+
+                            if(sort.value == "price"){
+                               if(sort.order == "asc")
+                                   products = products.sort( (a, b) => a.price - b.price)
+                               if(sort.order == "desc")
+                                   products = products.sort( (a, b) => b.price - a.price)
+                            }
+                            if(sort.value == "createdAt"){
+                                if(sort.order == "asc")
+                                    products = products.sort( (a, b) => new Date(a.createdAt) - new Date(b.createdAt) )
+                                if(sort.order == "desc")
+                                    products = products.sort( (a, b) => new Date(b.createdAt) - new Date(a.createdAt) )
+                            }
+                        }
+
+                        return products
+                    }
+                else
+                    return products
+                }
+            )
+
+
+            /*return Product.find(filterInput)
                 .then((result)=>{
                     console.log(`Products found`)
                     console.log(result)
@@ -36,7 +95,7 @@ const resolvers = {
                 .catch((err)=>{
                     console.error(`ERROR by finding all products: ${err}`)
                     return err
-                })
+                })*/
 
             /*if( args.sort.value == "price" &&  args.sort.order == "desc")
                 return [{
